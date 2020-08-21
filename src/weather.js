1ed8da11fd84c539cc7319e62e12f5b1adab0a7c
Frank Lai <frank810618@gmail.com>
2020-08-21 17:15:29 +0800

    finished project

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,10 @@
 import { ajax } from 'rxjs/ajax';
 import { fromEvent, BehaviorSubject, Subject, from, combineLatest } from "rxjs";
-import { tap, debounceTime, switchMap, skip, pluck, map } from "rxjs/operators";
+import { tap, debounceTime, switchMap, skipWhile, pluck, map } from "rxjs/operators";
 import { add } from "./helpers";
+
+const lastSearch = localStorage.getItem('lastSearch');
+const firstTerm = lastSearch ? lastSearch : '';
 
 // DOM elements
 const searchBox = document.getElementById('search');
@@ -13,14 +16,17 @@ const searchEvent = fromEvent(searchBox, 'keyup');
 const resultsEvent = fromEvent(resultBox, 'click');
 
 // Subjects: for multiple subscribers
-const inputSub = new BehaviorSubject('');
+const inputSub = new BehaviorSubject(firstTerm);
 const placeSub = new Subject();
 const weatherSub = new Subject();
 
+
+
 inputSub.pipe(
-    skip(1),
+    skipWhile(value => (!value || value.length < 3)),
     tap(_ => {
         spinner.className = 'spinner';
+        resultBox.innerHTML = '';
     }),
     debounceTime(1000),
     switchMap(searchTerm => {
@@ -34,11 +40,15 @@ inputSub.pipe(
         );
     })
 ).subscribe(result => {
+    localStorage.setItem('lastSearch', searchBox.value);
     add.result(result.description, result.place_id);
 });
 
 searchEvent.subscribe(event => {
-    resultBox.innerHTML = '';
+    const renderDiv = document.getElementById('renderDiv');
+    if (renderDiv) {
+        renderDiv.remove();
+    }
     inputSub.next(searchBox.value);
 });
 
@@ -70,11 +80,44 @@ combineLatest(weatherData, placeSub).subscribe(result => {
     const weather = result[0];
     const place = result[1];
     document.getElementById('image-container').innerHTML = '';
-    add.div(
-        `<div>
-            <p>Temperature: ${Math.round(weather.temp)}&deg;</p>
-            <p>Current Conditions: ${weather.icon}</p>
-            <p>Visibility: ${!!weather.visibility ?weather.visibility : 'N\\A' }</p>
-        </div>`
-    )
+    if (place.photos) {
+        const photos = place.photos;
+        const photoNum = Math.floor(Math.random() * photos.length);
+        add.div(
+            `
+            <div class="row">
+              <div class="col s12 m7">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photos[photoNum].photo_reference}&key=AIzaSyB6YDGhzImb7N1RKGlJdWulSm-FLaOQNHU">
+                    <div class="bg-gradient"></div>
+                    <span class="card-title">${place.formatted_address}</span>
+                  </div>
+                  <div class="card-content">
+                    <p>Temperature: ${Math.round(weather.temp)}&deg;</p>
+                    <p>Current Conditions: ${weather.icon}</p>
+                    <p>Visibility: ${!!weather.visibility ? weather.visibility : 'N\\A'}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            `
+        );
+    } else {
+        add.div(
+            `
+            <div class="row">
+                <div class="col s12 m6">
+                    <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                            <span class="card-title">${place.formatted_address}</span>
+                            <p>Temperature: ${Math.round(weather.temp)}&deg;</p>
+                            <p>Current Conditions: ${weather.icon}</p>
+                            <p>Visibility: ${!!weather.visibility ? weather.visibility : 'N\\A'}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `);
+    }
 });
