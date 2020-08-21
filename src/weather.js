df0fc66baff311ea923e321ad501e64c5cf8fd89
Frank Lai <frank810618@gmail.com>
2020-08-21 09:08:01 +0800

    preparations before starting

--- src/weather.js
+++ src/weather.js
@@ -1,3 +1,17 @@
-import { } from "rxjs";
+import { fromEvent, BehaviorSubject, Subject } from "rxjs";
 import { } from "rxjs/operators";
-import { add } from "./helpers";
+import { } from "./helpers";
+
+// DOM elements
+const searchBox = document.getElementById('search');
+const resultBox = document.getElementById('results-container');
+
+
+// Event Handlers
+const searchEvent = fromEvent(searchBox, 'keyup');
+const resultsEvent = fromEvent(resultBox, 'click');
+
+// Subjects: for multiple subscribers
+const inputSub = new BehaviorSubject('');
+const placeSub = new Subject();
+const weatherSub = new Subject();
