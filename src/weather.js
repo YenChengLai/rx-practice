b8786a66015384e8905eebc1a6efd905fc214c3d
Frank Lai <frank810618@gmail.com>
2020-08-21 10:22:16 +0800

    get place related results set

--- src/weather.js
+++ src/weather.js
@@ -1,11 +1,12 @@
-import { fromEvent, BehaviorSubject, Subject } from "rxjs";
-import { } from "rxjs/operators";
-import { } from "./helpers";
+import { ajax } from 'rxjs/ajax';
+import { fromEvent, BehaviorSubject, Subject, from } from "rxjs";
+import { tap, debounceTime, switchMap, skip } from "rxjs/operators";
+import { add } from "./helpers";
 
 // DOM elements
 const searchBox = document.getElementById('search');
 const resultBox = document.getElementById('results-container');
-
+const spinner = document.getElementById('spinner');
 
 // Event Handlers
 const searchEvent = fromEvent(searchBox, 'keyup');
@@ -15,3 +16,27 @@ const resultsEvent = fromEvent(resultBox, 'click');
 const inputSub = new BehaviorSubject('');
 const placeSub = new Subject();
 const weatherSub = new Subject();
+
+inputSub.pipe(
+    skip(1),
+    tap(_ => {
+        spinner.className = 'spinner';
+    }),
+    debounceTime(1000),
+    switchMap(searchTerm => {
+        return ajax.getJSON(`http://localhost:3000/autoComplete/${searchTerm}`).pipe(
+            tap(_ => {
+                spinner.className = '';
+            }),
+            switchMap(results => {
+                return from(results);
+            })
+        );
+    })
+).subscribe(result => {
+    add.result(result.description, result.place_id);
+});
+
+searchEvent.subscribe(event => {
+    inputSub.next(searchBox.value);
+});
